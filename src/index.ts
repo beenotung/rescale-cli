5247703b7701f15047adafd27cac2ff53faa3805
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-08-29 09:28:57 +0800

    feat: try to fix the remind/exceed value if possible

--- src/index.ts
+++ src/index.ts
@@ -25,5 +25,44 @@ export function rescale(options: { full: number; values: number[] }) {
     values[i] = value
   }
 
+  while (reminds != 0) {
+    let refs = values
+      .map((value, index) => ({
+        input: inputs[index],
+        value,
+        index,
+        diff: inputs[index] / total - value / full,
+      }))
+      .filter(ref => {
+        if (reminds > 0) {
+          return ref.diff > 0
+        }
+        if (reminds < 0) {
+          return ref.diff < 0
+        }
+      })
+
+    let max = refs.sort((a, b) => Math.abs(b.diff) - Math.abs(a.diff))[0]
+
+    let matches = refs.filter(ref => ref.diff == max.diff)
+
+    if (matches.length > Math.abs(reminds)) {
+      break
+    }
+
+    for (let match of matches) {
+      if (reminds > 0) {
+        reminds--
+        match.diff--
+        values[match.index]++
+      }
+      if (reminds < 0) {
+        reminds++
+        match.diff++
+        values[match.index]--
+      }
+    }
+  }
+
   return { reminds, values }
 }
